32e05af120c0b054d449eecd259c2047c34d5c9b
Scribani <scribani.andres@gmail.com>
2021-07-01 22:21:35 +0000

    Define start and goodbye method for ClinBoard class

--- clin_boards.rb
+++ clin_boards.rb
@@ -1,13 +1,34 @@
-require_relative "formatter"
-require_relative "requester"
+# require_relative "formatter"
+# require_relative "requester"
 
 class ClinBoards
-  def initialize
-    # Complete this
+  def initialize(_store = "store.json")
+    @store = "Store.new(store)" # HARDCODE!!!
+    @board = "@store.boards" # HARDCODE!!!
   end
 
   def start
-    # Complete this
+    action = ""
+    until action == "exit"
+      print "Enter action: "
+      action, id = gets.chomp.split # HARDCODE!!!
+      case action
+      when "create" then puts "create_board" # HARDCODE!!!
+      when "show" then puts "show_board(#{id})" # HARDCODE!!!
+      when "update" then puts "update_board(#{id})" # HARDCODE!!!
+      when "delete" then puts "remove_board(#{id})" # HARDCODE!!!
+      when "exit" then goodbye
+      else puts "Invalid option"
+      end
+    end
+  end
+
+  def goodbye
+    puts [
+      "\n####################################",
+      "#   Thanks for using CLIn Boards   #",
+      "####################################\n\n"
+    ]
   end
 end
 
