cba312a0e3db8f02b99f20a7d146bc740cb01703
Scribani <scribani.andres@gmail.com>
2021-07-01 23:29:48 +0000

    Add method to print board table

--- formatter.rb
+++ formatter.rb
@@ -9,25 +9,27 @@ module Formatter
       table.rows = collection.map(&row_formatter)
       puts table
     end
+
+    def self.print_boards
+      collection = [ # HARDCODE!!!
+        {
+          "id": 1,
+          "name": "Extended - CLIn Boards",
+          "description": "Task management for the last extended",
+          "lists": [0, 1, 2, 3]
+        },
+        {
+          "id": 2,
+          "name": "Test - CLIn Boards",
+          "description": "May Rubocop have mercy upon us",
+          "lists": [0, 1]
+        }
+      ]
+      print_content("CLIn Boards", %w[ID Name Description List(#cards)], collection) do |board| # HARDCODE!!!
+        [board[:id], board[:name], board[:description], "List(#{board[:lists].size})"] # HARDCODE!!!
+      end
+    end
   end
 end
 
-title = "CLIn Boards"
-headings = %w[ID Name Description List(#cards)]
-collection = [
-  {
-    "id": 1,
-    "name": "Extended - CLIn Boards",
-    "description": "Task management for the last extended",
-    "lists": [0, 1, 2, 3]
-  },
-  {
-    "id": 2,
-    "name": "Test - CLIn Boards",
-    "description": "May Rubocop have mercy upon us",
-    "lists": [0, 1]
-  }
-]
-Formatter::Test.print_content(title, headings, collection) do |board|
-  [board[:id], board[:name], board[:description], board[:lists].size]
-end
+Formatter::Test.print_boards
