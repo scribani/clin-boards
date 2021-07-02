2c6ef3e350a081d9818613a2656f2f554dcd6fdd
luisrc <lwrccodeable@gmail.com>
2021-07-02 15:16:18 +0000

    hot fix to_json, class list

--- list.rb
+++ list.rb
@@ -16,4 +16,12 @@ class List
   def update_list_name(new_name)
     @name = new_name
   end
+
+  def self.next_id
+    @id_secuence += 1
+  end
+
+  def to_json(*_args)
+    { id: @id, name: @name, cards: @cards }.to_json
+  end
 end
