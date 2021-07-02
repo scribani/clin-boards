9a16e5c9ec2f7fbd8a18c719f554a920e5d1f11d
luisrc <lwrccodeable@gmail.com>
2021-07-02 16:04:14 +0000

    Added methods create_card and delete_card

--- list.rb
+++ list.rb
@@ -13,12 +13,19 @@ class List
     @id_secuence += 1
   end
 
-  def update_list_name(new_name)
-    @name = new_name
+  def create_card(data)
+    new_card = Card.new(data)
+    @cards << new_card
   end
 
-  def self.next_id
-    @id_secuence += 1
+  def delete_card(id)
+    @cards.each_with_index do |card, index|
+      @cards.delete_at(index) if card.id == id
+    end
+  end
+
+  def update_list_name(new_name)
+    @name = new_name
   end
 
   def to_json(*_args)
